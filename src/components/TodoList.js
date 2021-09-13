4207290c4bb2817560976606b165e2d2456534af
zerofe13 <zerofe13@gmail.com>
2021-09-13 15:56:14 +0900

    onRemove

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
